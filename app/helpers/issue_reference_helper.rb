3c89d66c900ad7a12c75f198dad92c282d871caa
denvermullets <devmullets@gmail.com>
2026-04-10 08:48:07 -0400

    feat(issues): Create issue reference helper for server-side linkification

---
+++ app/helpers/issue_reference_helper.rb
@@ -0,0 +1,26 @@
+module IssueReferenceHelper
+  def linkify_issue_references(text, team)
+    return '' if text.blank?
+
+    issues_by_identifier = IssueReferenceParser.find_issues(text, team)
+                                               .index_by(&:identifier)
+
+    escaped = ERB::Util.html_escape(text)
+
+    escaped.gsub(IssueReferenceParser::ISSUE_REFERENCE_REGEX) do |match|
+      issue = issues_by_identifier[match]
+      if issue
+        link_to(match, team_issue_path(team, issue),
+                class: 'text-accent hover:underline font-mono text-sm')
+      else
+        match
+      end
+    end.html_safe
+  end
+
+  def format_with_issue_links(text, team)
+    return '' if text.blank?
+
+    simple_format(linkify_issue_references(text, team))
+  end
+end
