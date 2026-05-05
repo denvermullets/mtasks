293f2f09887ac280e2cba9dd3fe5ae9f080e2cb2
denvermullets <devmullets@gmail.com>
2026-05-05 12:24:59 -0400

    fix(session): tweak local session overwrite

--- test/test_helpers/session_test_helper.rb
+++ test/test_helpers/session_test_helper.rb
@@ -3,14 +3,14 @@ module SessionTestHelper
     Current.session = user.sessions.create!
 
     ActionDispatch::TestRequest.create.cookie_jar.tap do |cookie_jar|
-      cookie_jar.signed[:session_id] = Current.session.id
-      cookies['session_id'] = cookie_jar[:session_id]
+      cookie_jar.signed[:mtasks_session_id] = Current.session.id
+      cookies['mtasks_session_id'] = cookie_jar[:mtasks_session_id]
     end
   end
 
   def sign_out
     Current.session&.destroy!
-    cookies.delete('session_id')
+    cookies.delete('mtasks_session_id')
   end
 end
 
