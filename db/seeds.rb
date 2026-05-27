20fc0c62a028848c39c363de2419ccaba318bb85
denvermullets <devmullets@gmail.com>
2026-05-27 13:48:27 -0400

    fix(cable): idk

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ return unless Rails.env.development?
 
 # Create admin user
 admin = User.find_or_create_by!(email: "admin@example.com") do |user|
-  user.password = "password"
-  user.password_confirmation = "password"
+  user.password = "password123"
+  user.password_confirmation = "password123"
   user.name = "Admin User"
 end
 
