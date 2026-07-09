450750a152c3eb4004efc8642d7077348f26866a
denvermullets <devmullets@gmail.com>
2026-07-09 15:38:16 -0400

    fix: avoid db query on changelog read

--- app/controllers/changelog_controller.rb
+++ app/controllers/changelog_controller.rb
@@ -2,8 +2,6 @@ class ChangelogController < ApplicationController
   CHANGELOG_PATH = Rails.root.join('CHANGELOG.md').freeze
 
   def index
-    @body = Rails.cache.fetch('changelog:body', expires_in: 5.minutes) do
-      File.exist?(CHANGELOG_PATH) ? File.read(CHANGELOG_PATH) : ''
-    end
+    @body = File.exist?(CHANGELOG_PATH) ? File.read(CHANGELOG_PATH) : ''
   end
 end
